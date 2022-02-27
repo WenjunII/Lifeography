9884488ee62e7304df572f8c2f9652e17f9154a6
Wenjun II <wwenjunii@gmail.com>
2022-02-26 19:53:29 -0500

    Update dream.js

--- scripts/dream.js
+++ scripts/dream.js
@@ -1,5 +1,6 @@
 let img, img2, img3, img4, img5, img6, img7, img8;
 let showimg, showimg2, showimg3, showimg4, showimg5, showimg6, showimg7, showimg8;
+let positionX, positionY, positionX2, positionY2, positionX3, positionY3, positionX4, positionY4, positionX5, positionY5, positionX6, positionY6, positionX7, positionY7, positionX8, positionY8;
 
 function preload() {
 	img = loadImage('images/p5js/hand.png');
@@ -35,12 +36,29 @@ function setup() {
 
 function draw() {
   background(25, 25, 25);
-  image(img2, 0, 0);
-  image(img, 65, 7);
-  image(img8, 395, 15);
-  image(img5, 225, 267);
-  image(img4, 237, 284);
-  image(img3, 250, 300);
-  image(img7, 365, 98);
-  image(img6, 193, 70); 
+  frameRate(10);
+  image(img2, 0 + positionX2, 0 + positionY2);
+  image(img, 65 + positionX, 7 + positionY);
+  image(img8, 395 + positionX8, 15 + positionY8);
+  image(img5, 225 + positionX5, 267 + positionY5);
+  image(img4, 237 + positionX4, 284 + positionY4);
+  image(img3, 250 + positionX3, 300 + positionY3);
+  image(img7, 365 + positionX7, 98 + positionY7);
+  image(img6, 193 + positionX6, 70 + positionY6); 
+  positionX = random(-0.4, 0.4);
+  positionY = random(-0.4, 0.4);
+  positionX2 = random(-0.4, 0.4);
+  positionY2 = random(-0.4, 0.4);
+  positionX3 = random(-0.4, 0.4);
+  positionY3 = random(-0.4, 0.4);
+  positionX4 = random(-0.4, 0.4);
+  positionY4 = random(-0.4, 0.4);
+  positionX5 = random(-0.4, 0.4);
+  positionY5 = random(-0.4, 0.4);
+  positionX6 = random(-0.4, 0.4);
+  positionY6 = random(-0.4, 0.4);
+  positionX7 = random(-0.4, 0.4);
+  positionY7 = random(-0.4, 0.4);
+  positionX8 = random(-0.4, 0.4);
+  positionY8 = random(-0.4, 0.4);
 }
